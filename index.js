71b164af9642ffa4f050b4afd352cde5237ceddb
mrkrca <aleksa.djuric.dj@gmail.com>
2024-05-18 14:14:48 +0200

    inital commit

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const inputField = document.getElementById('input');
 
+var arrayOfLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var arrayOfOperators = ["Enter", "=", "*", "/", "-", "+"];
 
 
+$(document).on("keypress",function(event){
+    
+    if(arrayOfLetters.includes(event.key)){
+        console.log('Bruh... its a calculator.. use fucking numbers holy shits');
+        event.preventDefault();
+    } else if(event.key === "Enter"){
+        equalTo();
+        event.preventDefault();
+    } else {
+        inputField.textContent += event.key
+    }
+   
+})
 
 function insertNum(num){
     inputField.textContent += num
 }
-
 
 
 function equalTo(){
